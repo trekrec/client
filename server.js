f068bf99fb21bd7345c29088023790971285ea70
David Goldberg <drgold1988@gmail.com>
2015-10-18 23:37:46 -0700

    [Feature] creates basic express server

---
+++ server.js
@@ -0,0 +1,9 @@
+// var express = require('express');
+// var server = express();
+
+// var port = 8000;
+
+// server.use(express.static('public'));
+// server.listen(port, function() {
+//   console.log('Listening on port: ', port);
+// });

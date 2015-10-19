91f99578dd2214c8cf17fc2c36614b30adec9408
David Goldberg <drgold1988@gmail.com>
2015-10-18 23:37:15 -0700

    [Setup] updates gulpfile to include sourcemaps and babel

--- gulpfile.js
+++ gulpfile.js
@@ -84,3 +84,96 @@ gulp.task('default', ['build', 'serve', 'sass', 'watch']);
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+// var gulp = require('gulp');
+// var gutil = require('gulp-util');
+// var source = require('vinyl-source-stream');
+// var browserify = require('browserify');
+// var watchify = require('watchify');
+// var reactify = require('reactify');
+// var notifier = require('node-notifier');
+// var server = require('gulp-server-livereload');
+// var concat = require('gulp-concat');
+// var sass = require('gulp-sass');
+// var watch = require('gulp-watch');
+// var babel = require('gulp-babel');
+// var sourcemaps = require('gulp-sourcemaps');
+// var exit = require('gulp-exit');
+
+// var notify = function(error) {
+//   var message = 'In: ';
+//   var title = 'Error: ';
+
+//   if(error.description) {
+//     title += error.description;
+//   } else if (error.message) {
+//     title += error.message;
+//   }
+
+//   if(error.filename) {
+//     var file = error.filename.split('/');
+//     message += file[file.length-1];
+//   }
+
+//   if(error.lineNumber) {
+//     message += '\nOn Line: ' + error.lineNumber;
+//   }
+
+//   notifier.notify({title: title, message: message});
+// };
+
+
+// var bundler = watchify(browserify({
+//   entries: ['./src/app.jsx'],
+//   transform: [reactify],
+//   extensions: ['.jsx'],
+//   debug: true,
+//   cache: {},
+//   packageCache: {},
+//   fullPaths: true
+// }));
+
+// function bundle() {
+//   return bundler
+//     .bundle()
+//     .on('error', notify)
+//     .pipe(source('./public/main.js'))
+//     .pipe(gulp.dest('./'))
+// }
+// bundler.on('update', bundle)
+
+// gulp.task('build', function() {
+//   bundle()
+//   gulp.src('./public/main.js')
+//   .pipe(gulp.dest('./public'))
+// });
+
+// gulp.task('serve', function(done) {
+//   gulp.src('public')
+//     .pipe(server({
+//   livereload: {
+//         enable: true,
+//         filter: function(filePath, cb) {
+//           if(/main.js/.test(filePath)) {
+//             cb(true)
+//           } else if(/style.css/.test(filePath)){
+//             cb(true)
+//           }
+//         }
+//       },
+//       open: true
+//     }));
+// });
+
+// gulp.task('sass', function () {
+//   gulp.src('./sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(concat('style.css'))
+//     .pipe(gulp.dest('./public'));
+// });
+
+// gulp.task('default', ['build', 'serve', 'sass', 'watch']);
+
+// gulp.task('watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });

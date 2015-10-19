f41c03170648f473f82378aa73481ff4cf1c93e1
David Goldberg <drgold1988@gmail.com>
2015-10-18 23:49:40 -0700

    [Setup] sets up Profile component skeleton

---
+++ src/profile.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Profile = React.createClass({
+  render: function() {
+    return (
+      <div className="profile">
+        Profile Page
+      </div>
+    );
+  }
+});
+
+module.exports = Profile;
+

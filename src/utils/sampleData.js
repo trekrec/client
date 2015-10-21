15b25814a78b5f9da27ecc6b7f76467d3a3565e9
David Goldberg <drgold1988@gmail.com>
2015-10-21 15:07:34 -0700

    [Setup] todo, refactor to replace sample data with server data

---
+++ src/utils/sampleData.js
@@ -0,0 +1,17 @@
+var data = [
+  {
+    title: 'San Francisco',
+    description: 'Lands end and golden gate park are highlights'
+  },
+  {
+    title: 'Chicago',
+    description: 'Navy Pier and Oak Street Beach'
+  },
+  {
+    title: 'Galapagos',
+    description: 'Scuba all the way!'
+  }
+];
+
+module.exports = data;
+ 

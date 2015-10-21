5dfaaf8751e84b59709822669b6de697a5e98deb
David Goldberg <drgold1988@gmail.com>
2015-10-21 10:20:31 -0700

    [Fix] removes data parameter from makeBoardSpec

--- server/app/specs/makeBoardSpec.js
+++ server/app/specs/makeBoardSpec.js
@@ -13,10 +13,10 @@ describe('app logic', function() {
     });
 
     it('should make a new board with a title and description', function(done) {
-      var makeBoardResult = makeBoard('Chicago', 'The other greatest city in the world', {name: 'chi-test'});
+      var makeBoardResult = makeBoard('Chicago', 'The other greatest city in the world');
       var dbResult = db.select('*').from('boards').where({boardid: createBoardResult});
       dbResult[0].boardid.should.equal(createBoardResult);
-      dbResult[0].name.should.equal('chi-test');
+      // dbResult[0].name.should.equal('chi-test');
       done();
     });
     makeBoardResult();

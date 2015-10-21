21fdfebcc82ef838662182d6df39ea990cead9a9
David Goldberg <drgold1988@gmail.com>
2015-10-21 15:09:05 -0700

    [Feature] itinerary component passes data to BoardList

--- src/components/itinerary.jsx
+++ src/components/itinerary.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var NavBar = require('./navigation');
+var BoardList = require('./board-list');
+var data = require('../utils/sampleData');
 
 var Itinerary = React.createClass({
   render: function() {
     return (
       <div className="itinerary">
+        <NavBar />
         Itinerary Page
+        <BoardList data={data} />
       </div>
     );
   }

21a68ed07118cf3d5b6555f51d1d04ff46ba7b80
David Goldberg <drgold1988@gmail.com>
2015-10-23 11:24:45 -0700

    [Feature] provides API for accessing docStore queries

---
+++ server/db/documentStore/queries/documentStoreQueries.js
@@ -0,0 +1,7 @@
+var deleteData = require('./deleteData');
+var fetchData = require('./fetchData');
+var insertData = require('./insertData');
+
+exports.deleteData = deleteData;
+exports.fetchData = fetchData;
+exports.insertData = insertData;
